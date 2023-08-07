e1c161e65212b6573fd8548ddc3fe8a6cf4dc13e
weihao <weihaoysgs@gmail.com>
2023-08-07 19:29:24 +0800

    Update config

---
+++ cmake/packages.cmake
@@ -0,0 +1,17 @@
+# third party include path
+include_directories(${PROJECT_SOURCE_DIR}/thirdparty/eigen)
+include_directories(${PROJECT_SOURCE_DIR}/thirdparty/sophus)
+include_directories(${PROJECT_SOURCE_DIR}/thirdparty/g2o)
+include_directories(${PROJECT_SOURCE_DIR}/thirdparty/DBoW2)
+
+# OpenCV
+find_package(OpenCV 3.2.0 REQUIRED)
+include_directories(${OpenCV_INCLUDE_DIRS})
+
+# Pangolin
+find_package(Pangolin REQUIRED)
+include_directories(${Pangolin_INCLUDE_DIRS})
+
+set(g2o_libs ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_*.so)
+set(dbow_libs ${PROJECT_SOURCE_DIR}/thirdparty/DBoW2/lib/libDBoW2.so)
+set(pangolin_libs ${Pangolin_LIBRARIES} GL GLU GLEW glut)
