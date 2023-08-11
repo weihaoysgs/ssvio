9649110635d9c6cbb99f67caa922c24212285ed6
weihao <weihaoysgs@gmail.com>
2023-08-11 19:22:17 +0800

    Add Loop closeing files

---
+++ include/ssvio/loopclosing.hpp
@@ -0,0 +1,16 @@
+//
+// Created by weihao on 23-8-10.
+//
+
+#ifndef SSVIO_LOOPCLOSING_HPP
+#define SSVIO_LOOPCLOSING_HPP
+
+namespace ssvio {
+
+class loopclosing
+{
+};
+
+} // namespace ssvio
+
+#endif //SSVIO_LOOPCLOSING_HPP
---
+++ src/ssvio/loopclosing.cpp
@@ -0,0 +1,8 @@
+//
+// Created by weihao on 23-8-10.
+//
+
+#include "loopclosing.hpp"
+
+namespace ssvio {
+} // namespace ssvio
